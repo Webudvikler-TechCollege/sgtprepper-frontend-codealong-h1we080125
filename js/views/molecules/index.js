f8bfe8bdbda04c320c77c5d52349f39e4c9445d1
Heinz K <heka@techcollege.dk>
2025-10-22 10:50:17 +0200

    Added styles

--- js/views/molecules/index.js
+++ js/views/molecules/index.js
@@ -2,7 +2,7 @@ import { Heading } from "../atoms/index.js"
 
 export const HeaderView = () => {
     const element = document.createElement('header')
-    element.className = 'bg-slate-500 shadow-md border rounded-lg p-4 text-white'
+    element.className = 'bg-slate-500 p-4 text-white'
     const h1 = Heading('Sgt. Prepper')
     element.append(h1)
     return element
@@ -10,6 +10,7 @@ export const HeaderView = () => {
 
 export const MainView = (title, content) => {
     const element = document.createElement('main')
+    element.className = "p-4"
     const h1 = Heading(title)
     element.append(h1, content)
     return element
@@ -17,6 +18,7 @@ export const MainView = (title, content) => {
 
 export const FooterView = () => {
     const element = document.createElement('footer')
+    element.className = "bg-slate-500 p-4"
     element.innerHTML = `&copy; TECHCOLLEGE 2025`
     return element
 }
