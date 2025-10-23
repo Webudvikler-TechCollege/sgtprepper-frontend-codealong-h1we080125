88334d78c7ec1ba3982098d2a288db3e822a77a0
Heinz K <heka@techcollege.dk>
2025-10-23 13:57:23 +0200

    Added category list

--- js/controllers/categoryController.js
+++ js/controllers/categoryController.js
@@ -2,7 +2,17 @@ import { getList } from "../models/categoryModel.js"
 
 export const getCategoryList = async () => {
     const data = await getList()
-    console.log(data);
+
+    console.log(data)
+
+    const formattedCategories = data.map(item => ({
+        slug: item.slug,
+        title: item.title,
+        url: `/index.htm#/produkter?category=${item.slug}`
+    }))
     
-    return data
+    console.log(formattedCategories);
+    
+
+    return formattedCategories
 }
